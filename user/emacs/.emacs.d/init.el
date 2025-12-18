d237652a9782937598fc23477d344e64ac29fc36
ono ono <azazabc123@gmail.com>
2025-12-18 13:39:07 +0800

    feat: [emacs] disable useless scroll-bar-mode

--- user/emacs/.emacs.d/init.el
+++ user/emacs/.emacs.d/init.el
@@ -10,7 +10,7 @@
 ;; ======================================================
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-;; (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (setq inhibit-splash-screen t)
 
 ;; ======================================================
